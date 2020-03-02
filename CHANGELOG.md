46d53c55a786e749c21c1fc9cd0e79bf9f8fc547
Damien Calesse <damien.calesse@gmail.com>
2020-03-02 10:23:30 +0100

    Update require file and composer deps
    - Require file now search the directory containing the composer.json
      file to define the project directory path

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Require file now search for parent directory containing the composer.json
+
 ## [0.1.0] - 2020-02-28
 
 ### Added
--- src/require.php
+++ src/require.php
@@ -7,7 +7,15 @@ function requireComposerAutoload() {
 	foreach([ __DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php'] as $path) {
 		if (file_exists($path)) {
 			define('LINT_COMPOSER_FOLDER', dirname(realpath($path)));
-			define('LINT_ROOT_FOLDER', dirname(LINT_COMPOSER_FOLDER));
+			$root = LINT_COMPOSER_FOLDER;
+
+			do {
+				$root = dirname($root);
+			} while (!file_exists($root . DIRECTORY_SEPARATOR . 'composer.json') && ($root !== '/' && $root !== '\\'));
+
+			if ($root !== '/' && $root !== '\\') {
+				define('LINT_ROOT_FOLDER', $root);
+			}
 			return require_once($path);
 		}
 	}
